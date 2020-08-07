f8e7247884bfedeb4cfbe926d1cc118d32c9719d
Kyle Stough <kstough00@gmail.com>
2020-08-07 20:01:33 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,6 +10,13 @@ class EmailAddressParser
     @csv_emails = csv_emails
   end
   
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end 
+end 
   
   
   
